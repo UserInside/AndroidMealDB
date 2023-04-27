b2f9edb3a54c6021962e278f17f63b6dc0adcbe5
t1r <>
2023-04-27 21:47:26 +0300

    Refactoring WIP

--- app/src/main/java/com/example/mealdb/category/presentation/CategoryListViewModel.kt
+++ app/src/main/java/com/example/mealdb/category/presentation/CategoryListViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.mealdb.category.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import category.data.CategoryHttpClient
 import category.data.CategoryListGatewayImplementation
 import category.domain.CategoryListEntity
@@ -14,10 +17,20 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-class CategoryListViewModel : ViewModel() {
-    lateinit var interactor: CategoryListInteractor
+//TODO
+// Независимо от того, используете ли вы DI контейнеры или нет,
+// внедрение через конструктор (Constructor Injection) должен быть
+// первым способом управления зависимостями. Его использование не
+// только позволит сделать отношения между классами более явными,
+// но также позволит определить проблемы с дизайном, когда
+// количество параметров конструктора превысит определенную
+// границу. К тому же, все современные контейнеры внедрения зависимостей поддерживают данный паттерн.
+// https://habr.com/ru/articles/352530/
+class CategoryListViewModel(
+    private val interactor: CategoryListInteractor
+) : ViewModel() {
 
-    private val _stateFlow = MutableStateFlow<CategoryListUiState>(CategoryListUiState())
+    private val _stateFlow = MutableStateFlow(CategoryListUiState())
     val stateFlow: StateFlow<CategoryListUiState> = _stateFlow.asStateFlow()
 
     init {
@@ -43,13 +56,8 @@ class CategoryListViewModel : ViewModel() {
         }
     }
 
-
     suspend fun getCategoryEntity(): CategoryListEntity {
-        val categoryHttpClient = CategoryHttpClient()
-        val gateway: CategoryListGateway = CategoryListGatewayImplementation(categoryHttpClient)
-        interactor = CategoryListInteractor(gateway)
-        val data = interactor.fetchData()
-        return data
+        return interactor.fetchData()
     }
 
     fun getCategoryListSortedAscendingByName() : CategoryListEntity {
@@ -64,5 +72,11 @@ class CategoryListViewModel : ViewModel() {
         return interactor.filterCategoryList(query)
     }
 
-
+    companion object {
+        fun factory(
+            interactor: CategoryListInteractor,
+        ): ViewModelProvider.Factory = viewModelFactory {
+            initializer { CategoryListViewModel(interactor) }
+        }
+    }
 }
--- app/src/main/java/com/example/mealdb/category/presentation/MainCategoryListActivity.kt
+++ app/src/main/java/com/example/mealdb/category/presentation/MainCategoryListActivity.kt
@@ -1,43 +1,55 @@
 package com.example.mealdb.category.presentation
 
-import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.RadioButton
 import androidx.activity.OnBackPressedCallback
+import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import category.data.CategoryHttpClient
+import category.data.CategoryListGatewayImplementation
+import category.domain.CategoryListInteractor
 import com.example.mealdb.BottomSheetFragment
 import com.example.mealdb.ContentState
 import com.example.mealdb.R
 import com.example.mealdb.category.domain.CategoryListAdapter
 import com.example.mealdb.country.presentation.CountryListFragment
 import com.google.android.material.navigation.NavigationView
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
-class MainCategoryListActivity : AppCompatActivity(),
-    NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var drawer: DrawerLayout
-    private lateinit var adapter: CategoryListAdapter
-    private lateinit var viewModel: CategoryListViewModel
+class MainCategoryListActivity
+    : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private var drawer: DrawerLayout? = null
+    private var adapter: CategoryListAdapter? = null
+
+    //TODO https://developer.android.com/topic/libraries/architecture/viewmodel/viewmodel-factories
+    private val viewModel: CategoryListViewModel by viewModels {
+        CategoryListViewModel.factory(
+            CategoryListInteractor(
+                CategoryListGatewayImplementation(
+                    CategoryHttpClient()
+                )
+            )
+        )
+    }
 
     override fun onCreate(saveInstanceState: Bundle?) {
         super.onCreate(saveInstanceState)
@@ -52,7 +64,7 @@ class MainCategoryListActivity : AppCompatActivity(),
             this, drawer, appBar,
             R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
-        drawer.addDrawerListener(toggle)
+        drawer?.addDrawerListener(toggle)
         toggle.syncState()
 
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
@@ -62,48 +74,55 @@ class MainCategoryListActivity : AppCompatActivity(),
         val contentView = findViewById<View>(R.id.contentViewCategoryList)
         val progressBarView = findViewById<View>(R.id.includeProgressBar)
         val errorView = findViewById<View>(R.id.includeError)
+        val buttonRetry = findViewById<View>(R.id.buttonRetry)
 
-        viewModel = ViewModelProvider(this).get(CategoryListViewModel::class.java)
+        //create recyclerview
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_category)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this@MainCategoryListActivity)
+        adapter = CategoryListAdapter(
+            null,
+            this@MainCategoryListActivity
+        )
+        recyclerView.adapter = adapter
+
+        buttonRetry.setOnClickListener {
+            viewModel.fetchData()
+        }
 
         lifecycleScope.launch {
+            //TODO https://developer.android.com/topic/libraries/architecture/viewmodel#implement-viewmodel
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.stateFlow.collect {state ->
+                    when (state.contentState) {
+                        ContentState.Idle,
+                        ContentState.Loading -> {
+                            contentView.visibility = View.GONE
+                            progressBarView.visibility = View.VISIBLE
+                            errorView.visibility = View.GONE
+                        }
 
-            viewModel.stateFlow.onEach { state ->
-                when (state.contentState) {
-                    ContentState.Idle,
-                    ContentState.Loading -> {
-                        contentView.visibility = View.GONE
-                        progressBarView.visibility = View.VISIBLE
-                        errorView.visibility = View.GONE
+                        ContentState.Done -> {
+                            contentView.visibility = View.VISIBLE
+                            progressBarView.visibility = View.GONE
+                            errorView.visibility - View.GONE
+                        }
+
+                        ContentState.Error -> {
+                            contentView.visibility = View.GONE
+                            progressBarView.visibility = View.GONE
+                            errorView.visibility = View.VISIBLE
+                        }
                     }
-                    ContentState.Done -> {
-                        contentView.visibility = View.VISIBLE
-                        progressBarView.visibility = View.GONE
-                        errorView.visibility - View.GONE
-                    }
-                    ContentState.Error -> {
-                        contentView.visibility = View.GONE
-                        progressBarView.visibility = View.GONE
-                        errorView.visibility = View.VISIBLE
-                    }
+                    adapter?.setChangedCategoryEntity(state.categoryListEntity?.categoryList)
                 }
-
-                //create recyclerview
-                val recyclerView = findViewById<RecyclerView>(R.id.recycler_category)
-                recyclerView.setHasFixedSize(true)
-                recyclerView.layoutManager = LinearLayoutManager(this@MainCategoryListActivity)
-                adapter = CategoryListAdapter(
-                    state.categoryListEntity?.categoryList,
-                    this@MainCategoryListActivity
-                )
-                recyclerView.adapter = adapter
-
-            }.launchIn(lifecycleScope)
+            }
         }
 
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
-                if (drawer.isDrawerOpen(GravityCompat.START)) {
-                    drawer.closeDrawer(GravityCompat.START)
+                if (drawer?.isDrawerOpen(GravityCompat.START) == true) {
+                    drawer?.closeDrawer(GravityCompat.START)
                 } else {
 //                    this.remove()
                     finish()
@@ -136,7 +155,7 @@ class MainCategoryListActivity : AppCompatActivity(),
                     }
 
                     override fun onQueryTextChange(query: String?): Boolean {
-                        adapter.setChangedCategoryEntity(
+                        adapter?.setChangedCategoryEntity(
                             viewModel.getFilteredCategoryList(query).categoryList
                         )
                         return true
@@ -147,12 +166,12 @@ class MainCategoryListActivity : AppCompatActivity(),
             R.id.action_sort_category -> {
                 val bottomSheetFragment = BottomSheetFragment(
                     callbackSortAscendingByName = {
-                        adapter.setChangedCategoryEntity(
+                        adapter?.setChangedCategoryEntity(
                             viewModel.getCategoryListSortedAscendingByName().categoryList
                         )
                     },
                     callbackSortDescendingByName = {
-                        adapter.setChangedCategoryEntity(
+                        adapter?.setChangedCategoryEntity(
                             viewModel.getCategoryListSortedDescendingByName().categoryList
                         )
                     }
@@ -171,7 +190,7 @@ class MainCategoryListActivity : AppCompatActivity(),
         when (item.itemId) {
             R.id.nav_area -> {
                 if (areaFragment != null && areaFragment.isVisible) {
-                    drawer.closeDrawer(GravityCompat.START)
+                    drawer?.closeDrawer(GravityCompat.START)
                 } else {
                     supportFragmentManager.beginTransaction()
                         .replace(
@@ -187,28 +206,33 @@ class MainCategoryListActivity : AppCompatActivity(),
                     if (areaFragment.isVisible) {
                         supportFragmentManager.beginTransaction().remove(areaFragment).commit()
                     } else {
-                        drawer.closeDrawer(GravityCompat.START)
+                        drawer?.closeDrawer(GravityCompat.START)
                     }
                 } else {
-                    drawer.closeDrawer(GravityCompat.START)
+                    drawer?.closeDrawer(GravityCompat.START)
                 }
             }
             R.id.theme_light -> {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
                 recreate()
-                drawer.closeDrawer(GravityCompat.START)
+                drawer?.closeDrawer(GravityCompat.START)
 
             }
             R.id.theme_dark -> {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
                 recreate()
-                drawer.closeDrawer(GravityCompat.START)
+                drawer?.closeDrawer(GravityCompat.START)
             }
         }
 
-        drawer.closeDrawer(GravityCompat.START)
+        drawer?.closeDrawer(GravityCompat.START)
         return true
+    }
 
+    override fun onDestroy() {
+        drawer = null
+        adapter = null
+        super.onDestroy()
     }
 }
 
